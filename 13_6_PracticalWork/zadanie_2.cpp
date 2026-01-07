9c124172edd2f7dedf0db7190775400363f37b4f
Kurchin Ilya <kurchin.work@gmai.com>
2026-01-08 02:13:40 +0500

    Выполнил практику 13.6

--- 13_6_PracticalWork/zadanie_2.cpp
+++ 13_6_PracticalWork/zadanie_2.cpp
@@ -7,10 +7,26 @@
 using namespace std;
 
 int main() {
+
     vector<float> prices {2.5, 4.25, 3.0, 10.0};
     vector<int> items {1, 1, 0, 3};
 
-    for (int i = 0; i < items.size(); i++) {
-        элемент = элементу вектора
+    cout << "\nPrices on product:" << endl;
+    for (int i = 0; i < prices.size(); i++) {
+        cout << prices[i] << " ";
     }
+
+    cout << "\nCount products:" << endl;
+    for (int i = 0; i < items.size(); i++) {
+        cout << items[i] << " ";
+    }
+
+    float total = 0.0f;
+    for (int i = 0; i < items.size(); i++) {
+        if (items[i] >= 0 && items[i] <= prices.size()) {
+            total += prices[items[i]];
+        }
+    }
+
+    cout << "\nTotal cost: " << total;
 }
--- 13_6_PracticalWork/zadanie_3.cpp
+++ 13_6_PracticalWork/zadanie_3.cpp
@@ -1,3 +1,29 @@
-//
-// Created by kurch on 07.01.2026.
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> db(20);
+    int pos = 0;
+
+    while (true) {
+        int val;
+        cout << "input number: ";
+        cin >> val;
+
+        if (val == -1) break;
+
+        db[pos % 20] = val;
+        pos++;
+    }
+
+    cout << "output: ";
+    int start = (pos >= 20) ? pos % 20 : 0;
+    int count = min(pos, 20);
+
+    for (int i = 0; i < count; i++) {
+        cout << db[(start + i) % 20] << " ";
+    }
+    cout << endl;
+}
