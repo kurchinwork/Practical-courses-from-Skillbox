21d71c7c861a7e0de28ffe93d238659a02e7e70c
Илья Курчин <2568313406@my.lerna.ru>
2026-01-28 12:22:43 +0500

    Выполнил практику 17.4

--- 17_5_PracticalWork/zadanie_3.cpp
+++ 17_5_PracticalWork/zadanie_3.cpp
@@ -4,19 +4,47 @@
 #include <iostream>
 using namespace std;
 
-bool findSubstring (const char* firstString, const char* secondString) {
-    int numbOfMatches = 0;
+bool findSubstring(const char* firstString, const char* secondString) {
     for (int i = 0; firstString[i] != '\0'; i++) {
-        if (firstString[i] == secondString[i])
-            numbOfMatches++;
-        if (numbOfMatches == secondString.lenght())
+        int j = 0;
+        while (secondString[j] != '\0' && firstString[i + j] == secondString[j]) {
+            j++;
+        }
+        if (secondString[j] == '\0') {
             return true;
+        }
     }
+    return false;
 }
+        /*
+        int numbOfMatches = 0;
+        for (int i = 0; firstString[i] != '\0'; i++) {
+            for (int j = 0; secondString[j] != '\0'; j++) {
+                if (firstString[i] == secondString[j])
+                    numbOfMatches++;
+            }
+        }
+        if (numbOfMatches == 0) {
+            return false;
+        } else {
+            return true;
+        }*/
+
 
 int main () {
     const char* a = "Hello world";
     const char* b = "wor";
     const char* c = "banana";
 
+    if (findSubstring (a, b) == true) {
+        cout << "\nThe second string is a substring of the first: "<< a << ", " << b <<endl;
+    }
+    if (findSubstring (a, c) == true) {
+        cout << "\nThe third string is a substring of the first: "<< a << ", " << c <<endl;
+    }
+    if ((findSubstring (a, b) == false) && (findSubstring (a, c) == false)) {
+        cout << "\nNone of the expressions is a substring of the first!" << a << ", " << b << ", " << c <<endl;
+    }
+
+    return 0;
 }
