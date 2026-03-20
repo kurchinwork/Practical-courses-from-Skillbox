05dd36c1292b3fc64ac8c9f5f2f0a3875a4f97d2
kurchin_iu <kurchin.ilya99@gmail.com>
2026-03-20 17:44:19 +0500

    Practical is done

--- 26_OOPpart1/2_PhoneSim.cpp
+++ 26_OOPpart1/2_PhoneSim.cpp
@@ -1,3 +1,178 @@
 //
-// Created by kurchin_iu on 17.03.2026.
+// Created by kurchin_iu on 20.03.2026.
 //
+#include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class SIMcard {
+
+    private:
+        string nameSubscriber = "Unknown";
+        string subscriberNumber = "+79379379379";
+    map <string, string> subscriberList;
+
+public:
+    void AddInJournal(string& subscriber, string& subscriberNumber) {
+        subscriberList[subscriberNumber] = subscriber;
+    }
+    void AddSubscriber () {
+        bool validNum = false;
+        do {
+            validNum = false;
+            cout << "\nEnter subscriber name: ";
+            getline(cin, nameSubscriber);
+
+            // проверка имени на пустоту
+            if (nameSubscriber.empty()) {
+                cout << "\nName cannot be empty! Try again." << endl;
+            }
+        } while (nameSubscriber.empty());
+
+        do {
+            cout << "\nEnter subscriber number: ";
+            cin >> subscriberNumber; cin.ignore();
+
+            if (subscriberNumber.length() != 12) {
+                cout << "\nThe number length must be 12 characters."
+                        "\nFor example:+79379379379"
+                        "\nTry again.";
+            } else if (subscriberNumber[0] != '+' || subscriberNumber[1] != '7') {
+                cout << "\nThe phone code must start with +7";
+                validNum = true;
+                } else {
+                    bool hasInvalidChar = false;
+                    for (int i = 2; i < subscriberNumber.length(); i++) {
+                        if (subscriberNumber[i] < '0' || subscriberNumber[i] > '9') {
+                            cout << "\nThe number must contain only integers."
+                                    "\nSimbol '" << subscriberNumber[i] << "' is invalid";
+                            hasInvalidChar = true;
+                            validNum = true;
+                            break;
+                        }
+                    }
+                    // все проверки пройдены
+                    if (!hasInvalidChar) {
+                        validNum = false;
+                    }
+                }
+        } while(validNum);
+        AddInJournal(nameSubscriber, subscriberNumber);
+        cout << "\nSubscriber added successfully!" << endl;
+    }
+
+    // имя по номеру
+    string getNameByNumber(const string& number) {
+        if (subscriberList.find(number) != subscriberList.end()) {
+            return subscriberList[number];  // или subscriberList.at(number)
+        }
+        return "";
+    }
+
+    //  номер по имени
+    string getNumberByName(const string& name) {
+        for (const auto& entry : subscriberList) {
+            if (entry.second == name) {
+                return entry.first;  // возвращаем номер
+            }
+        }
+        return "";
+    }
+};
+
+class PhoneSimulator {
+    private:
+    string smsText = "";
+
+    SIMcard* currentSIMcard = nullptr;
+public:
+    void setSim(SIMcard* simcard) {
+        currentSIMcard = simcard;
+    }
+    void calling () {
+        string dataForCall = "";
+        cout << "\nEnter the subscriber's number or name."
+                "\nEnter:"; getline(cin, dataForCall);
+
+        if (!dataForCall.empty()) {
+            if (dataForCall[0] == '+') {
+                string name = currentSIMcard->getNameByNumber(dataForCall);
+                if (!name.empty()) {
+                    cout << "\nCall to " << name << " with num: " << dataForCall << endl;
+                } else {
+                    cout << "\nThere is no such contact in your address book." << endl;
+                }
+            } else {
+                string number = currentSIMcard->getNumberByName(dataForCall);
+                if (!number.empty()) {
+                    cout << "\nCall to " << number << " a " << dataForCall << endl;
+                } else {
+                    cout << "\nThere is no such contact in your address book." << endl;
+                }
+            }
+        }
+
+        }
+    void smsing () {
+        string dataForCall = "";
+        cout << "\nEnter the subscriber's number or name."
+                "\nEnter:"; getline(cin, dataForCall);
+
+        if (!dataForCall.empty()) {
+            if (dataForCall[0] == '+') {
+                string name = currentSIMcard->getNameByNumber(dataForCall);
+                if (!name.empty()) {
+                    cout << "\nMessage to " << name << " with num: " << dataForCall << endl;
+                    cout << "\nEnter message:"; getline(cin, smsText);
+                } else {
+                    cout << "\nThere is no such contact in your address book." << endl;
+                }
+            } else {
+                string number = currentSIMcard->getNumberByName(dataForCall);
+                if (!number.empty()) {
+                    cout << "\nMessage to " << number << " a " << dataForCall << endl;
+                    cout << "\nEnter message:"; getline(cin, smsText);
+                } else {
+                    cout << "\nThere is no such contact in your address book." << endl;
+                }
+            }
+        }
+
+    }
+};
+
+void listCommands() {
+    cout << "\nEnter command: "
+        "\n~add - to add a phone number to your address book;"
+        "\n~call - to call the number;"
+        "\n~sms - for writing SMS;"
+        "\n```````````````````````"
+        "\n~command - for list all all commands Adio player;"
+        "\n~exit - for exit Adio player." << endl;
+}
+
+
+
+int main() {
+    SIMcard SIMcard;
+    PhoneSimulator phoneSimulator;
+    phoneSimulator.setSim(&SIMcard);
+
+    listCommands();
+    string command = "";
+    do {
+        cout << "\nEnter command: "; cin >> command; cin.ignore();
+        if (command == "add") {
+            SIMcard.AddSubscriber();
+        }
+        if (command == "call") {
+            phoneSimulator.calling();
+        }
+        if (command == "sms") {
+            phoneSimulator.smsing();
+        }
+    } while(command != "exit");
+
+    return 0;
+}
--- 26_OOPpart1/3_WindowControl.cpp
+++ 26_OOPpart1/3_WindowControl.cpp
@@ -1,3 +1,116 @@
 //
-// Created by kurchin_iu on 17.03.2026.
+// Created by kurchin_iu on 20.03.2026.
 //
+#include <iostream>
+
+using namespace std;
+
+class Window {
+private:
+    int total_Height = 50,
+        total_Width = 80;
+
+    int init_Height = 0,
+        init_Width = 0;
+
+    int startCoordH = 0,
+        startCoordW = 0;
+
+    int moveHeight = 0,
+        moveWidht = 0;
+
+    bool move = false,
+         resize = true;
+
+public:
+    void out() {
+        for (int y = 0; y < total_Height; y++) {
+            for (int x = 0; x < total_Width; x++) {
+                if (resize) {
+                    // границы окна
+                    bool inWindowX = (x >= startCoordW && x < startCoordW + init_Width);
+                    bool inWindowY = (y >= startCoordH && y < startCoordH + init_Height);
+
+                    if (inWindowX && inWindowY) {
+                        // внутри окна
+                        if (x == startCoordW || x == startCoordW + init_Width - 1 ||
+                            y == startCoordH || y == startCoordH + init_Height - 1) {
+                            cout << "#";  // граница окна
+                            } else {
+                                cout << " ";  // Внутренность окна
+                            }
+                    } else {
+                        cout << ".";
+                    }
+                } else {
+                    cout << ".";
+                }
+            }
+            cout << endl;
+        }
+    }
+    void resized() {
+        cout << "\nEnter Height window: ";cin >> init_Height;
+        cout << "\nEnter Width window: ";cin >> init_Width;
+
+        cout << "\nEnter start coord H: "; cin >> startCoordH;
+        cout << "\nEnter start coord W: "; cin >> startCoordW;
+        cin.ignore();
+        bool parametrs = startCoordH >= 0
+                      && startCoordW >= 0
+                      && startCoordH < total_Height
+                      && startCoordW < total_Width
+                      && startCoordH + init_Height <= total_Height
+                      && startCoordW + init_Width <= total_Width;
+
+        if (parametrs) {
+            resize = true;
+        } else {
+            resize = false;
+            cout << "\nYour window parameters are out of range." << endl;
+        }
+    }
+    void moved() {
+        cout << "\nEnter move Height window: ";cin >> moveHeight;
+        cout << "\nEnter move Width window: ";cin >> moveWidht;
+
+        bool parametrs = moveHeight >= 0 && moveWidht >=0
+                      && moveHeight < total_Height
+                      && moveWidht < total_Width
+                      && startCoordH + init_Height + moveHeight <= total_Height
+                      && startCoordW + init_Width + moveWidht <= total_Width;
+        if (parametrs) {
+            move = true;
+        }
+    }
+};
+void listCommands() {
+    cout << "\nEnter command: "
+        "\n~move - to move the window;"
+        "\n~resize - to scale the window;"
+        "\n~display - to display the window;"
+        "\n```````````````````````"
+        "\n~command - for list all all commands Adio player;"
+        "\n~close - for exit." << endl;
+}
+
+int main() {
+    Window window;
+    listCommands();
+    string command = "";
+    do {
+
+        cout << "\nEnter command: "; cin >> command; cin.ignore();
+        if (command == "move") {
+            window.moved();
+        }
+        if (command == "resize") {
+            window.resized();
+        }
+        if (command == "display") {
+            window.out();
+        }
+    } while(command != "close");
+
+    return 0;
+}
